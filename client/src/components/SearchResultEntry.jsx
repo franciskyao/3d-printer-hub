3b6726d6c6d3c255381132bc9f43e279638383ee
Francis Yao <neko.makie27@gmail.com>
2021-07-08 10:09:25 -0700

    feat: add function to save search result to db

--- client/src/components/SearchResultEntry.jsx
+++ client/src/components/SearchResultEntry.jsx
@@ -1,22 +1,42 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function SearchResultEntry(props) {
-  // console.log(props.entry)
+  const [ category, setCategory] = useState(null)
+  const { name, public_url, preview_image, like_count, comment_count, creator } = props.entry;
+  const { name: author } = creator;
 
-  //need propped down addTodb
-  //need name
-  //need public_url
-  //preview_image
-  //like_count
-  //comment_count
-  //creator.name
+  function addToDb() {
+    console.log('Add data to db');
+    axios.post('/update', {
+      name: name,
+      public_url, public_url,
+      preview_image: preview_image,
+      category, category
+    })
+      .then((success) => {
+        props.updateList();
+      })
+      .catch(() => console.log('Failed to add in DB'))
+  }
+
   return (
     <div>
-      <select id="projectCategory">
-        <option value="extruder">extruder</option>
-        <option value="hotend">hotend</option>
-        <option value="project">project</option>
-      </select>
+      <div>
+        <p><b>{name}</b></p>
+        <img src={preview_image} alt={name}></img>
+        <p>Likes: {like_count}</p>
+        <p>Comments: {comment_count}</p>
+      </div>
+      <div>
+        <select id="projectCategory" onChange={(e) => setCategory(e.target.value)}>
+          <option></option>
+          <option value="extruder">extruder</option>
+          <option value="hotend">hotend</option>
+          <option value="project">project</option>
+        </select>
+        <input type="button" onClick={addToDb}value="Save"></input>
+      </div>
     </div>
   )
 }
