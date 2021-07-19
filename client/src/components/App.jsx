eef75b7fd399bfcbb489366fb8210e2571ebf8fd
Francis Yao <neko.makie27@gmail.com>
2021-07-19 16:55:12 -0700

    build: Removed sass. Added material-ui

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,7 +6,7 @@ import HotendList from './HotendList.jsx';
 import ExtruderList from './ExtruderList.jsx';
 import ProjectList from './ProjectList.jsx';
 import mockData from './mockData.js';
-import '../style.scss';
+import '../style.css';
 
 function App() {
   const [dbList, setDbList] = useState(null);
