ff8b3c6c724d33a7144510a8669e9a4dd6b978c0
Francis Yao <neko.makie27@gmail.com>
2021-07-20 19:42:27 -0700

    added app bar with search input from material-ui

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,6 +8,27 @@ import ProjectList from './ProjectList.jsx';
 import mockData from './mockData.js';
 import '../style.css';
 import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+  },
+  drawer: {
+    width: drawerWidth
+  },
+  searchInput: {
+    height: 40,
+    width: drawerWidth,
+    backgroundColor: "white",
+  }
+}));
 
 function App() {
   const [dbList, setDbList] = useState(null);
@@ -15,6 +36,8 @@ function App() {
   const [hotendList, setHotendList] = useState(null);
   const [extruderList, setExtruderList] = useState(null);
   const [projectList, setProjectList] = useState(null);
+  const [mainDisplay, setMainDisplay] = useState(null);
+  const classes = useStyles();
 
   useEffect(() => {
     updateList()
@@ -47,15 +70,30 @@ function App() {
   return (
     <>
       {/* top naviation to cycle through list.  */}
-      <Grid>
-      {/* Nav bar on top cycles through the list */}
+      <AppBar position="fixed" className={classes.appBar}>
+        {/* 3D Printer Hub */}
+        {/* <Search setDbList={setDbList} updateList={updateList}/> */}
+        {/* <Typography className={classes.title} variant="h6" noWrap>
+          Material-UI
+        </Typography> */}
+        <OutlinedInput className={classes.searchInput}/>
+      </AppBar>
+      {/* <Drawer className={classes.drawer}>
+      </Drawer> */}
+      <Grid container lg={12}>
+        <Grid id="monitor"  lg={2}>
+          <PrinterList />
+        </Grid>
+        <Grid id="mainDisplay" lg={7}>
+          This is going to be main display
+        </Grid>
+        <Grid id="list" lg={3}>
+          {extruderList && <ExtruderList extruderList={extruderList} updateList={updateList}/>}
+          {hotendList &&<HotendList hotendList={hotendList} updateList={updateList}/>}
+          {projectList && <ProjectList projectList={projectList} updateList={updateList}/>}
+        </Grid>
       </Grid>
       <div id="main">
-        <Search setDbList={setDbList} updateList={updateList}/>
-        <PrinterList />
-        {extruderList && <ExtruderList extruderList={extruderList} updateList={updateList}/>}
-        {hotendList &&<HotendList hotendList={hotendList} updateList={updateList}/>}
-        {projectList && <ProjectList projectList={projectList} updateList={updateList}/>}
       </div>
     </>
   )
