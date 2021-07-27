125910d590bb434c7ebec2307b59eb96ab5c5563
Francis Yao <neko.makie27@gmail.com>
2021-07-26 21:02:48 -0700

    changed styling to use material-ui

--- client/src/components/PrinterList.jsx
+++ client/src/components/PrinterList.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react'
 import PrinterEntry from './PrinterEntry.jsx'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  textField: {
+    width: '25ch',
+  },
+  addButton: {
+    width: '25ch',
+  },
+}));
 
 function PrinterList () {
   const [ printerList, setPrinterList ] = useState([])
   const [ printerIp, setPrinterIp] = useState(null)
+  const classes = useStyles();
 
   const addPrinter = function () {
     console.log(`Printer ${printerIp} added`)
@@ -16,9 +29,25 @@ function PrinterList () {
 
   return (
     <div id="printerList">
-      <label>Enter Printer IP:</label><br />
-        <input type="text" onChange={(e) => setPrinterIp(e.target.value)}></input>
-        <input type="button" onClick={addPrinter} value="Add Printer"></input>
+      <TextField
+        className={classes.textField}
+        required
+        label="3D printer address"
+        id="targetLength"
+        margin="dense"
+        placeholder="http://10.0.0.133/"
+        variant="outlined"
+        onChange={(e) => setPrinterIp(e.target.value)}
+      />
+      <br />
+      <Button
+        className={classes.addButton}
+        onClick={addPrinter}
+        variant="contained"
+        color="primary">
+          Add Printer
+      </Button>
+      <br />
       <h2>Active Printers</h2>
       {printerList && printerList.map((printer) => <PrinterEntry printer={printer} key={printer}/>)}
     </div>
