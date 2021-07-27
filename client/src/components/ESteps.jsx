1b46d5d4f080e3c97c122ba0ac253cdf0618a039
Francis Yao <neko.makie27@gmail.com>
2021-07-26 20:49:05 -0700

    required all input before calculation

--- client/src/components/ESteps.jsx
+++ client/src/components/ESteps.jsx
@@ -7,13 +7,14 @@ const useStyles = makeStyles((theme) => ({
   textField: {
     width: '25ch',
   },
-  calculate: {
-    // width: '25ch'
+  calculateButton: {
+    width: '25ch',
   },
 }));
 
 function ESteps (props) {
   const [finalEsteps, setFinalEsteps] = useState(null);
+  // const [currentEsteps, setCurrentEsteps] = useState(null);
   const classes = useStyles();
 
   const handleCalculateButton = function() {
@@ -22,44 +23,54 @@ function ESteps (props) {
     const measured = parseInt(measuredLength.value);
     const newEsteps = current * (measured/target)
     setFinalEsteps(newEsteps)
+    // setCurrentEsteps(newEsteps)
   };
 
   return (
     <>
-        <TextField
-          label="Current E-steps"
-          id="currentEsteps"
-          type="number"
-          className={classes.textField}
-          helperText="Enter m503 command"
-          margin="dense"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          label="Target Length"
-          id="targetLength"
-          type="number"
-          className={classes.textField}
-          helperText="Enter a length"
-          margin="dense"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          label="Actual Measured Length"
-          id="measuredLength"
-          type="number"
-          defaultValue="Default Value"
-          className={classes.textField}
-          helperText="Use a ruler"
-          margin="dense"
-          variant="outlined"
-        />
-        <br />
-        <br />
-        <Button className={classes.calculate} onClick={handleCalculateButton}>Calculate</Button>
-        Your new e-steps is: {finalEsteps}
+      {/* {currentEsteps && <p>Your current esteps is {currentEsteps}</p>} */}
+      <TextField
+        required
+        label="Current E-steps"
+        id="currentEsteps"
+        type="number"
+        className={classes.textField}
+        helperText="Enter m503 command"
+        margin="dense"
+        variant="outlined"
+      />
+      <br />
+      <TextField
+        required
+        label="Target Length"
+        id="targetLength"
+        type="number"
+        className={classes.textField}
+        helperText="Enter a length"
+        margin="dense"
+        variant="outlined"
+      />
+      <br />
+      <TextField
+        required
+        label="Measured Length"
+        id="measuredLength"
+        type="number"
+        defaultValue="Default Value"
+        className={classes.textField}
+        helperText="Use a ruler"
+        margin="dense"
+        variant="outlined"
+      />
+      <br />
+      <br />
+      <Button className={classes.calculateButton}
+        onClick={handleCalculateButton}
+        variant="contained"
+        color="primary">
+          Calculate
+      </Button>
+      <p>Your new e-steps is: {finalEsteps && finalEsteps}</p>
     </>
   )
 }
