10de629b62fdb26286e23d7105e11e12597b3041
Francis Yao <neko.makie27@gmail.com>
2021-07-26 20:50:47 -0700

    removed duplicate id. added state for ip onchange

--- client/src/components/PrinterList.jsx
+++ client/src/components/PrinterList.jsx
@@ -3,26 +3,24 @@ import PrinterEntry from './PrinterEntry.jsx'
 
 function PrinterList () {
   const [ printerList, setPrinterList ] = useState([])
+  const [ printerIp, setPrinterIp] = useState(null)
+
   const addPrinter = function () {
-    const printerAddress = address.value;
-    address.value = ''
-    console.log(`Printer ${printerAddress} added`)
-    if (printerList.indexOf(printerAddress) === -1) {
+    console.log(`Printer ${printerIp} added`)
+    if (printerList.indexOf(printerIp) === -1) {
       const tempList = printerList.slice();
-      tempList.push(printerAddress);
+      tempList.push(printerIp);
       setPrinterList(tempList);
     }
   }
 
   return (
     <div id="printerList">
-    <br />
-    <br />
-      <label>Enter Printer Address:</label><br />
-        <input type="text" id="address"></input>
+      <label>Enter Printer IP:</label><br />
+        <input type="text" onChange={(e) => setPrinterIp(e.target.value)}></input>
         <input type="button" onClick={addPrinter} value="Add Printer"></input>
       <h2>Active Printers</h2>
-      {printerList && printerList.map((printer) => <PrinterEntry printer={printer} id={printer}/>)}
+      {printerList && printerList.map((printer) => <PrinterEntry printer={printer} key={printer}/>)}
     </div>
   )
 }
