14c612389e1dd624a5adcebcb862c03db2a16b74
Francis Yao <neko.makie27@gmail.com>
2021-07-28 23:35:05 -0700

    created BLTouch component

--- client/src/components/BLTouch.jsx
+++ client/src/components/BLTouch.jsx
@@ -1,5 +1,12 @@
-const BLTouch = function() {
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
+const BLTouch = function() {
+  return (
+    <>
+      Steps to configure BLTouch in Marlin
+    </>
+  )
 }
 
 export default BLTouch;
