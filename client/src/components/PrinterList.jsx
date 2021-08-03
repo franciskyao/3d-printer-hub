4f1f83daf609b1ff6994817b003fce4fb0e20ade
Francis Yao <neko.makie27@gmail.com>
2021-08-02 23:26:55 -0700

    removed adding printer function and buttons

--- client/src/components/PrinterList.jsx
+++ client/src/components/PrinterList.jsx
@@ -3,6 +3,7 @@ import PrinterEntry from './PrinterEntry.jsx'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   textField: {
@@ -28,8 +29,8 @@ function PrinterList () {
   }
 
   return (
-    <div id="printerList">
-      <TextField
+    <Grid container spacing={2} id="printerList">
+      {/* <TextField
         className={classes.textField}
         required
         label="3D printer address"
@@ -48,9 +49,13 @@ function PrinterList () {
           Add Printer
       </Button>
       <br />
-      <h2>Active Printers</h2>
-      {printerList && printerList.map((printer) => <PrinterEntry printer={printer} key={printer}/>)}
-    </div>
+      <h2>Active Printers</h2> */}
+      {printerList && printerList.map((printer) =>
+        <Grid item lg={4}>
+          <PrinterEntry printer={printer} key={printer}/>
+        </Grid>
+      )}
+    </Grid>
   )
 }
 
