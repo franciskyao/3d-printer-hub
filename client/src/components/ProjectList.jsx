39ce1caa63534b733e913f18bf847f57c5736629
Francis Yao <neko.makie27@gmail.com>
2021-08-06 02:49:30 -0700

    renamed imports of ProjectEntry

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import ProjectEntry from './ProjectEntry.jsx'
+import ProjectListEntry from './ProjectListEntry.jsx'
 import List from '@material-ui/core/List';
 
 function ProjectList (props) {
   return (
     <List id="projectList">
       <h1>Project List</h1>
-      {props.projectList.map((project) => <ProjectEntry project={project} key={project.id} updateList={props.updateList} />)}
+      {props.projectList.map((project) => <ProjectListEntry project={project} key={project.id} updateList={props.updateList} />)}
     </List>
   )
 }
