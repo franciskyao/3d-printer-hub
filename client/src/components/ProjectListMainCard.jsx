2507827ab9f6a65d41e2376f6e845e3bf490d093
Francis Yao <neko.makie27@gmail.com>
2021-08-09 23:11:54 -0700

    added thumbnail images to card

--- client/src/components/ProjectListMainCard.jsx
+++ client/src/components/ProjectListMainCard.jsx
@@ -6,14 +6,29 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 
+const useStyles = makeStyles((theme) => ({
+  media: {
+    width: 100,
+    height:100,
+  },
+}));
+
 const ProjectListMainCard = function(props) {
   const { project } = props;
   const { proj_name: projName, preview_image: previewImage } = project
+  const classes = useStyles();
+
   console.log(project)
+  console.log(classes)
+
   return (
     <Card>
     <CardHeader
       title={projName}
+    />
+    <CardMedia
+      image={previewImage}
+      className={classes.media}
     />
     </Card>
   )
