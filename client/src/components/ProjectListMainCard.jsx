5957277a9c506c768ed346b2d0ef0cd65e1392b3
Francis Yao <neko.makie27@gmail.com>
2021-08-09 22:56:34 -0700

    removed card component imports

---
+++ client/src/components/ProjectListMainCard.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+
+const ProjectListMainCard = function(props) {
+  const { project } = props;
+  const { proj_name: projName } = project
+  console.log(project)
+  return (
+    <Card>
+      {projName}
+    </Card>
+  )
+}
+
+export default ProjectListMainCard;
