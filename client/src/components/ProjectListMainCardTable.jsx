3ead1c0d7aa66cc40f5c5103a49c7483c1ca6220
Francis Yao <neko.makie27@gmail.com>
2021-08-10 21:22:56 -0700

    created component to handle table data

---
+++ client/src/components/ProjectListMainCardTable.jsx
@@ -0,0 +1,22 @@
+import mockPart from './mockPart.js';
+import React, { useState } from 'react';
+
+const ProjectListMainCardTable = function(props) {
+  // id: 2,
+  // project_id: 3,
+  // part_name: "m3 screws",
+  // part_available: 6,
+  // part_needed: 3,
+  // part_complete: false
+
+  const addPart = function () {
+
+  }
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default ProjectListMainCardTable;
