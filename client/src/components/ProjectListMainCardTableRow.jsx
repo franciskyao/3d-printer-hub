cba77133162eb61d0013d16445e2f37e7e64e711
Francis Yao <neko.makie27@gmail.com>
2021-08-10 21:40:07 -0700

    created new component to handle changes in parts amount

---
+++ client/src/components/ProjectListMainCardTableRow.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const ProjectListMainCardTableRow = function(props) {
+  const { part } = props;
+  return (
+    <>
+    </>
+  )
+
+}
+
+export default ProjectListMainCardTableRow;
