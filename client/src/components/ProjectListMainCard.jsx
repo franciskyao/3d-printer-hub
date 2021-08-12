7139af54280c047e3c062c9a25d6fb32e2c0b716
Francis Yao <neko.makie27@gmail.com>
2021-08-11 22:52:06 -0700

    added button and logic to expand card part table

--- client/src/components/ProjectListMainCard.jsx
+++ client/src/components/ProjectListMainCard.jsx
@@ -5,9 +5,10 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
 import ProjectListMainCardTable from './ProjectListMainCardTable.jsx';
-
-import mockPart from './mockPart.js';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 const useStyles = makeStyles((theme) => ({
   media: {
@@ -34,7 +35,12 @@ const ProjectListMainCard = function(props) {
       image={previewImage}
       className={classes.media}
     />
-    {shouldExpandPartsTable ? <ProjectListMainCardTable partsList={partsList}/> : null}
+    <CardActions>
+      <IconButton onClick={()=> (setShouldExpandPartsTable(!shouldExpandPartsTable))}>
+        {shouldExpandPartsTable ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+      </IconButton>
+    </CardActions>
+    {shouldExpandPartsTable ? <ProjectListMainCardTable /> : null}
     </Card>
   )
 }
