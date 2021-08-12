4c01cffbf6e50250b58ed3c2f46038aad326044a
Francis Yao <neko.makie27@gmail.com>
2021-08-12 13:50:30 -0700

    generated table rows from mock data

--- client/src/components/ProjectListMainCardTableRow.jsx
+++ client/src/components/ProjectListMainCardTableRow.jsx
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 
 const ProjectListMainCardTableRow = function(props) {
+  const { project_id: projectId, part_name: partName, part_available: partAvailable, part_needed: partNeeded, part_complete: partComplete } = props.part;
+  // id: 2,
+  // project_id: 3,
+  // part_name: "m3 screws",
+  // part_available: 6,
+  // part_needed: 3,
+  // part_complete: false
   const { part } = props;
   return (
-    <>
-    </>
+    <TableRow>
+      <TableCell>{partName}</TableCell>
+      <TableCell>{partAvailable}</TableCell>
+      <TableCell>{partNeeded}</TableCell>
+      <TableCell>{partComplete}</TableCell>
+      <TableCell></TableCell>
+    </TableRow>
   )
 
 }
