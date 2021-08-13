592df8e6d6f1f71332d73f05f74e3a39c03e1cab
Francis Yao <neko.makie27@gmail.com>
2021-08-13 01:38:41 -0700

    changed Completed column to show difference or check

--- client/src/components/ProjectListMainCardTableRow.jsx
+++ client/src/components/ProjectListMainCardTableRow.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import CheckIcon from '@material-ui/icons/Check';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
 
 const ProjectListMainCardTableRow = function(props) {
-  const { project_id: projectId, part_name: partName, part_available: partAvailable, part_needed: partNeeded, part_complete: partComplete } = props.part;
+  const { id: partId, project_id: projectId, part_name: partName, part_available: partAvailable, part_needed: partNeeded, part_complete: partComplete } = props.part;
+
+  console.log('this is part id', partId)
 
   const handleDeleteButton = function() {
-    //delete request
     console.log('I delete this part from db');
     console.log('I update the part list')
   }
@@ -22,7 +25,7 @@ const ProjectListMainCardTableRow = function(props) {
       <TableCell>{partName}</TableCell>
       <TableCell>{partAvailable}</TableCell>
       <TableCell>{partNeeded}</TableCell>
-      <TableCell>{partComplete}</TableCell>
+      <TableCell>{partAvailable === partNeeded ? <CheckIcon /> : <span>Need {partNeeded - partAvailable}</span>}</TableCell>
       <TableCell>
         <IconButton onClick={handleDeleteButton}>
           <DeleteIcon />
