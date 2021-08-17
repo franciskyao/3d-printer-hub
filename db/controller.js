e1ae85c8f01c651c6fea98806575e1fce586325c
Francis Yao <neko.makie27@gmail.com>
2021-08-16 20:51:46 -0700

    create controller to remove A part

--- db/controller.js
+++ db/controller.js
@@ -38,14 +38,20 @@ const getProjectParts = function(id, res) {
     .catch((err) => console.log(`Failed to get parts of project ${id}`))
 }
 
-const removeParts; = function(id, res) {
-  pool.query(`Delete FROM projectParts WHERE project_id = ${}`)
+const removeAllParts = function(projectId, res) {
+  pool.query(`Delete FROM projectParts WHERE project_id = ${projectId}`)
     .then((success) => res.send(success))
-    .catch((err) => console.log(`Failed to delete parts of project ${id}`))
+    .catch((err) => console.log(`Failed to delete parts of project ${projectId}`))
+}
+
+const removeAPart = function(partId, res) {
+  pool.query(`Delete FROM projectParts WHERE id = ${partId}`)
+    .then((success) => res.send(success))
+    .catch((err) => console.log(`Failed to delete parts of project ${partId}`))
 }
 
 module.exports.getAll = getAll;
 module.exports.addProject = addProject;
 module.exports.removeProject = removeProject;
 module.exports.getProjectParts = getProjectParts;
-module.exports.removeParts = removeParts;
+module.exports.removeAllParts = removeAllParts;
