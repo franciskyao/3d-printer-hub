014622f6800a2dc461244dcd1e5b19730dd0685d
Francis Yao <neko.makie27@gmail.com>
2021-08-23 21:36:25 -0700

    created component to handle search results in main

---
+++ client/src/components/SearchMain.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import SearchResultEntry from './SearchResultEntry.jsx'
+
+const SearchMain = function(props) {
+  return (
+    <>
+      displays search results
+    </>
+  )
+}
+
+export default SearchMain;
