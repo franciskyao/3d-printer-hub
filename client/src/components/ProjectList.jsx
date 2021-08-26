0842661a661ca5a66fd81912fd64a1528bae3798
Francis Yao <neko.makie27@gmail.com>
2021-08-26 14:49:03 -0700

    changed to be a reuseable component. Added uppercase function for first letter

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 import ProjectListEntry from './ProjectListEntry.jsx'
 import List from '@material-ui/core/List';
 
+const capitalizeFirstLetter = function(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
 function ProjectList (props) {
+
   return (
     <List id="projectList">
-      <h1>Project List</h1>
+      <h1>{capitalizeFirstLetter(props.category)}</h1>
       {props.projectList.map((project) => <ProjectListEntry project={project} key={project.id} updateList={props.updateList} />)}
     </List>
   )
