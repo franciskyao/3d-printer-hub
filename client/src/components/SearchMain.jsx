3fdcdf5f247065149b67c0de8190101ecdade648
Francis Yao <neko.makie27@gmail.com>
2021-09-04 23:40:35 -0700

    created navigation for search

--- client/src/components/SearchMain.jsx
+++ client/src/components/SearchMain.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import SearchResultEntry from './SearchResultEntry.jsx'
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 const SearchMain = function(props) {
   return (
@@ -13,6 +19,25 @@ const SearchMain = function(props) {
             key={entry.id}/>
           </Grid>
         ))}
+        <Grid item md={12}>
+          <Button variant="contained" size="large" color="primary">
+            <ArrowBackIosIcon />
+            <ArrowBackIosIcon />
+          </Button>
+
+          <Button variant="contained" size="large" color="primary">
+            <ArrowBackIcon />
+          </Button>
+
+          <Button variant="contained" size="large" color="primary">
+            <ArrowForwardIcon />
+          </Button>
+
+          <Button variant="contained" size="large" color="primary">
+            <ArrowForwardIosIcon />
+            <ArrowForwardIosIcon />
+          </Button>
+        </Grid>
     </Grid>
   )
 }
