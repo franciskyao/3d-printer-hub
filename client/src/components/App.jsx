415b2c9b235f0484d4a01dd45e112995b6ee4de8
Francis Yao <neko.makie27@gmail.com>
2021-09-08 20:22:23 -0700

    code clean up to airbnb style

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Search from './Search.jsx';
 import PrinterAdd from './PrinterAdd.jsx';
 import PrinterList from './PrinterList.jsx';
 import ProjectList from './ProjectList.jsx';
@@ -10,45 +9,15 @@ import Menu from './Menu.jsx';
 import BLTouch from './BLTouch.jsx';
 import SearchMain from './SearchMain.jsx';
 import '../style.css';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import AppBar from '@material-ui/core/AppBar';
-import Drawer from '@material-ui/core/Drawer';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import SearchIcon from '@material-ui/icons/Search';
-import Typography from '@material-ui/core/Typography';
-
-
-import mockData from './mockData.js';
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    margin: 0
-  },
-  spacer: {
-    height: 56
-  },
-  drawer: {
-    width: 250
-  },
-  searchInput: {
-    height: 40,
-    width: 250,
-    backgroundColor: "white",
-  }
-}));
 
 function App() {
   const [dbList, setDbList] = useState(null);
   const [printerList, setPrinterList] = useState([]);
-  const [projectList, setProjectList] = useState(null);
   const [searchResultList, setSearchResultList] = useState(null)
   const [mainDisplay, setMainDisplay] = useState(null);
-  const [listDisplay, setListDisplay] = useState(null);
-  const [projectCategories, setProjectCategories] = useState(null);
   const [searchEntry, setSearchEntry] = useState(null);
   const [searchPage, setSearchPage] = useState(1);
-  const classes = useStyles();
 
   useEffect(() => {
     updateList();
@@ -92,7 +61,7 @@ function App() {
       .then((models) => {
         setDbList(models.data.rows);
       })
-      .catch((err) => console.log('Failed to get projects'))
+      .catch(() => console.log('Failed to get projects'))
   }
 
   return (
@@ -101,7 +70,6 @@ function App() {
         <Grid container>
           <Menu
             changeMainDisplay={changeMainDisplay}
-            projectCategories={projectCategories}
             search={search}
             changeSearchPage={changeSearchPage}
           />
