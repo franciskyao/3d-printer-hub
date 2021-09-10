e5b3af17fae45ff58a5a3b9b0fdd9cecaea42aa2
Francis Yao <neko.makie27@gmail.com>
2021-09-10 15:20:17 -0700

    added prop types and removed unused state

--- client/src/components/PrinterList.jsx
+++ client/src/components/PrinterList.jsx
@@ -1,31 +1,22 @@
-import React, { useState } from 'react'
-import PrinterEntry from './PrinterEntry.jsx'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import PrinterEntry from './PrinterEntry.jsx';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
-
-const useStyles = makeStyles((theme) => ({
-  textField: {
-    width: '25ch',
-  },
-  addButton: {
-    width: '25ch',
-  },
-}));
-
 function PrinterList (props) {
 
   return (
     <Grid container spacing={2} id="printerList">
       {props.printerList && props.printerList.map((printer) =>
-        <Grid item lg={4}>
-          <PrinterEntry printer={printer} key={printer}/>
+        <Grid item lg={4}  key={printer}>
+          <PrinterEntry printer={printer}/>
         </Grid>
       )}
     </Grid>
-  )
+  );
 }
 
 export default PrinterList;
+
+PrinterList.propTypes = {
+  printerList: PropTypes.array,
+};
