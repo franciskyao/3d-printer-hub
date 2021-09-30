1d8e038e8b4793922fa86f35d416619963301e85
Francis Yao <neko.makie27@gmail.com>
2021-09-29 22:49:31 -0700

    changed component to be a Grid container

--- client/src/components/ProjectListMain.jsx
+++ client/src/components/ProjectListMain.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ProjectListMainCard from './ProjectListMainCard.jsx';
 import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
 
 const ProjectListMain = function(props) {
   const {projectList} = props;
   return (
-    <>
+    // <>
+    <Grid container spacing={1}>
     {projectList.map(project => (
       <ProjectListMainCard project={project} key={project.id} />
     ))}
-    </>
+    </Grid>
+    // </>
   );
 };
 
